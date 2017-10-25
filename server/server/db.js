21d00541d15844602bf73c9149ae4ea97018312f
Sanni Taiwo <tywosanni@gmail.com>
2017-10-25 21:55:56 +0100

    {#152292328] Create a dummy data

---
+++ server/server/db.js
@@ -0,0 +1,39 @@
+const db = [];
+
+db.recipes = [
+  {
+    id: 1,
+    userId: 1,
+    recipeName: 'Pounded Yam',
+    mealType: 'Lunch',
+    level: 'Hard',
+    description: 'blah blah blah',
+    ingredients: ['yam', 'salt', 'water'],
+    upvotes: 30,
+    downvotes: 4,
+  },
+  {
+    id: 2,
+    userId: 3,
+    recipeName: 'Jollof rice',
+    mealType: 'Lunch',
+    level: 'Hard',
+    description: 'blah blah blah',
+    ingredients: ['rice', 'pepper', 'water'],
+    upvotes: 1,
+    downvotes: 40,
+  },
+];
+
+db.review = [
+  {
+    id: 1,
+    recipeId: 1,
+    review: "It's so so delicious"
+  },
+  {
+    id: 2,
+    recipeId: 2,
+    review: 'not tasty'
+  }
+];
