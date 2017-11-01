4eb57dc267608812522726ab3162535540683cc5
Sanni Taiwo <tywosanni@gmail.com>
2017-11-01 20:21:52 +0100

    [#152485544] Import the method to post review to a controller method

--- server/server/controllers/reviewController.js
+++ server/server/controllers/reviewController.js
@@ -1,33 +1,20 @@
-import db from '../db';
-/**
- * @class reviewController
-*/
-export default class review {
+import { reviews, Recipes } from '../models';
+import reviewHelper from '../helper/review';
+
+export default {
   /**
-   * get  all Recipe
+   * Post reviews
    * @param {object} req
    * @param {object} res
    * @returns  {JSON} Returns a JSON object
    */
-  static addReview(req, res) {
-    if (!req.body.review) {
+  add(req, res) {
+    const id = req.params.recipeId;
+    if (isNaN(id)) {
       res.status(400).send({
-        Message: 'Please enter Review'
-      });
-    } else {
-      const len = db.review.length;
-      const id = 1 + len;
-      db.review.push({
-        id,
-        recipeId: req.params.Id,
-        userId: req.body.userId,
-        review: req.body.review
-      });
-      res.status(201).send({
-        success: true,
-        review: db.review[id - 1],
-        message: 'Review Added'
+        message: 'Parameter should be a nuber'
       });
     }
+    reviewHelper.addReview(req, res, Recipes, reviews);
   }
-}
+};
