24ec8cdef3c17756922282e86c4abeb086237ab4
Sanni Taiwo <tywosanni@gmail.com>
2017-11-02 01:54:32 +0100

    [#152490932] Import the mehod to favorite recipe to a controller method

---
+++ server/server/controllers/favoriteController.js
@@ -0,0 +1,15 @@
+import { favorites, Recipes } from '../models';
+import favoriteHelper from '../helper/favorite';
+
+export default {
+  add(req, res) {
+    const id = req.params.userId;
+    if (isNaN(id)) {
+      res.status(400).send({
+        message: 'Parameter should be a number'
+      });
+    }
+    favoriteHelper.create(req, res, Recipes, favorites);
+  }
+};
+
