af63cdac578978bb68d0ccae31e3ba432d3cbed1
Sanni Taiwo <tywosanni@gmail.com>
2017-11-02 17:30:19 +0100

    [#152507102] Add more test cases

--- server/server/test/tests.js
+++ server/server/test/tests.js
@@ -105,7 +105,7 @@ describe('More-Recipe Tests:', () => {
         });
     });
   });
-  describe('Creating and updating data', () => {
+  describe('Test for Recipe', () => {
     it('POST /api/v1/recipes does create   recipe', (done) => {
       chai.request(app)
         .post('/api/v1/recipes/')
@@ -124,96 +124,68 @@ describe('More-Recipe Tests:', () => {
           done();
         });
     });
-  });
-  describe('Deleting data', () => {
-    it('Delete /api/v1/recipes?:recipeId does get all recipes', (done) => {
+    it('POST /api/v1/recipes does create   recipe', (done) => {
       chai.request(app)
-        .delete('/api/v1/recipes/3')
+        .post('/api/v1/recipes/')
+        .set('x-access-token', token)
+        .type('form')
+        .send({
+          recipeName: 'Goatsoup',
+          mealType: 'breakfast',
+          ingredients: 'water, beans,hfj',
+          description: 'Buy the meat from market',
+          method: 'fry the yam for five minutes'
+        })
+        .end((err, res) => {
+          expect(res.status).equal(201);
+          expect(res.body.message).equal('Recipe successfully added');
+          done();
+        });
+    });
+    it('PUT /api/v1/recipes/recipeId does update recipe', (done) => {
+      chai.request(app)
+        .put('/api/v1/recipes/1')
+        .set('x-access-token', token)
+        .type('form')
+        .send({
+          recipeName: 'Meat',
+        })
         .end((err, res) => {
           expect(res.status).equal(200);
-          expect(res.body.message).equal('Recipe has been Deleted');
-          expect(res.body.message).to.be.a('string');
+          expect(res.body.message).equal('Recipe successfully updated');
+          done();
+        });
+    });
+    it('DELETE /api/v1/recipes/recipeId does delete recipe', (done) => {
+      chai.request(app)
+        .delete('/api/v1/recipes/2')
+        .set('x-access-token', token)
+        .type('form')
+        .end((err, res) => {
+          expect(res.status).equal(200);
+          expect(res.body.message).equal('recipe successfully deleted');
+          done();
+        });
+    });
+    it('GET /api/v1/recipes does get all recipe', (done) => {
+      chai.request(app)
+        .get('/api/v1/recipes')
+        .set('x-access-token', token)
+        .end((err, res) => {
+          expect(res.status).equal(200);
+          expect(res.body).to.be.an('array');
+          done();
+        });
+    });
+    it('GET /api/v1/recipes does get a recipe', (done) => {
+      chai.request(app)
+        .get('/api/v1/recipes/1')
+        .set('x-access-token', token)
+        .end((err, res) => {
+          expect(res.status).equal(200);
+          expect(res.body.id).equal(1);
           done();
         });
     });
   });
-  describe('Validating input', () => {
-    it('POST /api/v1/recipes valid input for recipeName', (done) => {
-      chai.request(app)
-        .post('/api/v1/recipes/')
-        .send({
-          userId: 5,
-          recipeName: '',
-          mealType: 'lunch',
-          description: 'Boil water for five minutes',
-          ingredients: 'water, goat, rice, blah,',
-        })
-        .end((err, res) => {
-          expect(res.status).equal(400);
-          expect(res.body.message).equal('Please Enter Recipe Name');
-          done();
-        });
-    });
-    it('POST /api/v1/recipes valid input for mealType', (done) => {
-      chai.request(app)
-        .post('/api/v1/recipes/')
-        .send({
-          userId: 5,
-          recipeName: 'Beans',
-          mealType: '',
-          description: 'Boil water for five minutes',
-          ingredients: 'water, goat, rice, blah,',
-        })
-        .end((err, res) => {
-          expect(res.status).equal(400);
-          expect(res.body.message).equal('Please Enter the mealType');
-          done();
-        });
-    });
-    it('POST /api/v1/recipes valid input for description', (done) => {
-      chai.request(app)
-        .post('/api/v1/recipes/')
-        .send({
-          userId: 5,
-          recipeName: 'Beans',
-          mealType: 'lunch',
-          description: '',
-          ingredients: 'water, goat, rice, blah,',
-        })
-        .end((err, res) => {
-          expect(res.status).equal(400);
-          expect(res.body.message).equal('Please Enter the description to make recipe');
-          done();
-        });
-    });
-    it('POST /api/v1/recipes valid input for ingredients', (done) => {
-      chai.request(app)
-        .post('/api/v1/recipes/')
-        .send({
-          userId: 5,
-          recipeName: 'Beans',
-          mealType: 'lunch',
-          description: 'Boil water for ten minutes',
-          ingredients: '',
-        })
-        .end((err, res) => {
-          expect(res.status).equal(400);
-          expect(res.body.message).equal('Please Enter  Ingridents required');
-          done();
-        });
-    });
-    it('POST /api/v1/recipes/:recipeId/reviews validate input for reviews', (done) => {
-      chai.request(app)
-        .post('/api/v1/recipes/3/reviews')
-        .send({
-          userId: 5,
-          review: ""
-        })
-        .end((err, res) => {
-          expect(res.status).equal(400);
-          expect(res.body.Message).equal('Please enter Review');
-          done();
-        });
-    });
-  });  
 });
