d279094aee7902def229e1844dc039929241686d
Sanni Taiwo <tywosanni@gmail.com>
2017-11-07 17:35:08 +0100

    Add more test cases

--- server/server/test/tests.js
+++ server/server/test/tests.js
@@ -104,6 +104,51 @@ describe('More-Recipe Tests:', () => {
           done();
         });
     });
+    it('returns 400 error when email is invalid', (done) => {
+      chai.request(app)
+        .post('/api/v1/users/signup')
+        .type('form')
+        .send({
+          username: 'boruto',
+          email: 'ecample.com',
+          password: 'naruto'
+        })
+        .end((err, res) => {
+          expect(res.status).equal(400);
+          expect(res.body.error).equal('This email is invalid');
+          done();
+        });
+    });
+    it('returns 400 error if user already exist', (done) => {
+      chai.request(app)
+        .post('/api/v1/users/signup')
+        .type('form')
+        .send({
+          username: 'Thywo',
+          email: 'eample.@gmail.com',
+          password: 'naruto'
+        })
+        .end((err, res) => {
+          expect(res.status).equal(400);
+          expect(res.body.error).equal('This username already exist');
+          done();
+        });
+    });
+    it('returns 400 error if email already exist', (done) => {
+      chai.request(app)
+        .post('/api/v1/users/signup')
+        .type('form')
+        .send({
+          username: 'boruto',
+          email: 'example@gmail.com',
+          password: 'naruto'
+        })
+        .end((err, res) => {
+          expect(res.status).equal(400);
+          expect(res.body.error).equal('This email already exists');
+          done();
+        });
+    });
   });
   describe('Test for Recipe', () => {
     it('POST /api/v1/recipes does create   recipe', (done) => {
@@ -156,6 +201,20 @@ describe('More-Recipe Tests:', () => {
           done();
         });
     });
+    it('PUT /api/v1/recipes/recipeId returns error if recipe does not exist', (done) => {
+      chai.request(app)
+        .put('/api/v1/recipes/4')
+        .set('x-access-token', token)
+        .type('form')
+        .send({
+          recipeName: 'Meat',
+        })
+        .end((err, res) => {
+          expect(res.status).equal(404);
+          expect(res.body.message).equal('Recipe not found');
+          done();
+        });
+    });
     it('DELETE /api/v1/recipes/recipeId does delete recipe', (done) => {
       chai.request(app)
         .delete('/api/v1/recipes/2')
@@ -164,6 +223,17 @@ describe('More-Recipe Tests:', () => {
         .end((err, res) => {
           expect(res.status).equal(200);
           expect(res.body.message).equal('recipe successfully deleted');
+          done();
+        });
+    });
+    it('DELETE /api/v1/recipes/recipeId returns error if recipe does not exist', (done) => {
+      chai.request(app)
+        .delete('/api/v1/recipes/4')
+        .set('x-access-token', token)
+        .type('form')
+        .end((err, res) => {
+          expect(res.status).equal(404);
+          expect(res.body.message).equal('Recipe not found');
           done();
         });
     });
@@ -201,7 +271,7 @@ describe('More-Recipe Tests:', () => {
           done();
         });
     });
-    it('POST /api/v1/recipes/:recipeId/reviews does create review for a recipe', (done) => {
+    it('returns error for empty review', (done) => {
       chai.request(app)
         .post('/api/v1/recipes/1/reviews')
         .set('x-access-token', token)
@@ -211,6 +281,43 @@ describe('More-Recipe Tests:', () => {
         .end((err, res) => {
           expect(res.body.message).equal('Review cannot be empty');
           expect(res.status).equal(400);
+          done();
+        });
+    });
+    it('returns error for recipe that does not exist', (done) => {
+      chai.request(app)
+        .post('/api/v1/recipes/4/reviews')
+        .set('x-access-token', token)
+        .send({
+          review: 'It is delicious'
+        })
+        .end((err, res) => {
+          expect(res.body.message).equal('Recipe not found');
+          expect(res.status).equal(404);
+          done();
+        });
+    });
+    it('POST /api/v1/users/:userId/recipes does create favorite recipe for user', (done) => {
+      chai.request(app)
+        .post('/api/v1/users/1/recipes')
+        .set('x-access-token', token)
+        .send({
+          recipeId: 1
+        })
+        .end((err, res) => {
+          expect(res.body.message).equal('Recipe Favorited');
+          expect(res.status).equal(201);
+          expect(res.body.favorited).to.be.an('object');
+          done();
+        });
+    });
+    it('GET /api/v1/users/:userId/recipes does get all favorite recipe for user', (done) => {
+      chai.request(app)
+        .get('/api/v1/users/1/recipes')
+        .set('x-access-token', token)
+        .end((err, res) => {
+          expect(res.status).equal(200);
+          expect(res.body).to.be.an('array');
           done();
         });
     });
