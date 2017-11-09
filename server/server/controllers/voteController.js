a9d7a5604bb0e9ad144f2d670c0d9274ee8557c5
Sanni Taiwo <tywosanni@gmail.com>
2017-11-09 14:03:18 -0800

    Add the controller for upvoting and downvoting

--- server/server/controllers/voteController.js
+++ server/server/controllers/voteController.js
@@ -1,19 +1,47 @@
-import db from '../db';
+import { votes } from '../models';
+import recipe from '../helper/recipe';
+import voter from '../helper/downvotes';
 
-/**
- * @class votes
-*/
-export default class votes {
-  /**
-   *
-   * @param {object} req
-   * @param {object} res
-   * @returns  {JSON} Returns a JSON object
-   */
-  static getMostVotedRecipe(req, res) {
-    const compareFunct = ((a, b) => a.upvotes < b.upvotes);
-    const upvoted = db.recipes.sort(compareFunct);
-    return res.status(200).send(upvoted);
+export default {
+  upvote(req, res) {
+    votes
+      .find({
+        where: {
+          userId: req.decoded.id,
+          recipeId: req.params.recipeId,
+        }
+      })
+      .then((found) => {
+        if (found.upvotes === true) {
+          return res.status(400).send({
+            message: 'You have already upvoted this recipe'
+          });
+        }
+        recipe.checkExistingRecipe(req, res);
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
+  },
+  downvote(req, res) {
+    votes
+      .find({
+        where: {
+          userId: req.decoded.id,
+          recipeId: req.params.recipeId,
+        }
+      })
+      .then((found) => {
+        if (found.downvotes === true) {
+          return res.status(400).send({
+            message: 'You have already downvoted this recipe'
+          });
+        }
+        if (found.upvotes === true && found.downvotes === false) {
+          voter.alreadyUpvoted(req, res);
+        }
+        recipe.findRecipe(req, res);
+      })
+      .catch(err => res.status(400).send(err));
   }
-}
-
+};
