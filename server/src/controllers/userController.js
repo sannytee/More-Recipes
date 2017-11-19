ceb374b927de7879310d5f5962eec4e19c0f5694
Sanni Taiwo <tywosanni@gmail.com>
2017-11-18 21:24:51 -0800

    Refactor userController and account class

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,7 +1,3 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import Sequelize from 'sequelize';
-import { Users } from '../models';
 import account from '../helper/account';
 
 export default {
@@ -12,14 +8,7 @@ export default {
    * @returns  {JSON} Returns success or failure message
    */
   signup(req, res) {
-    // Gets username email and password from body
-    const { username, email } = req.body;
-    let { password } = req.body;
-    if (password) {
-      password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-    }
-    // Create User with provided info
-    account.createUser(req, res, Users, username, password, email);
+    account.createUser(req, res);
   },
   /**
    * Enables Users to signin into account
@@ -28,8 +17,7 @@ export default {
    * @returns  {JSON} Returns success or failure message
    */
   signIn(req, res) {
-    const { Op } = Sequelize;
-    account.signInUser(req, res, Users, bcrypt, jwt, Op);
+    account.signInUser(req, res);
   }
 };
 
