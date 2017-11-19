f730378af54556615a4e349ef9bc433352497879
Sanni Taiwo <tywosanni@gmail.com>
2017-11-18 18:56:57 -0800

    Change the reference path for router

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
-import routes from './src/routes';
+import router from './src/routes';
 
 // Set up the express application
 const app = express();
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-routes(app);
+router(app);
 // set up a default catch-all route
 app.get('*', (req, res) => res.status(400).send({
   message: 'Resource cannot be found'
