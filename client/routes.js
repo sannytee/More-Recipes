b4e2f3b3379343f48a9c4b9ed7a701fe62986d88
Sanni Taiwo <tywosanni@gmail.com>
2017-11-24 10:22:18 +0100

    [#153102519] Add the route to signup

--- client/routes.js
+++ client/routes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import LandingPage from './components/LandingPage/index';
+import SignupPage from './components/SignupPage/index';
 
 export default (
   <Route path='/' component={App}>
-    <IndexRoute component={ LandingPage } />
+    <IndexRoute component={LandingPage}/>
+    <Route path='signup' component={SignupPage}/>
   </Route>
 );
