cc99cf656465b469a8ff1c92a2da243909b697c7
Sanni Taiwo <tywosanni@gmail.com>
2017-11-25 16:51:25 +0100

    Add the route to signin

--- client/routes.js
+++ client/routes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import LandingPage from './components/LandingPage/index';
+import SigninPage from './components/SigninPage/index';
 
 export default (
   <Route path='/' component={App}>
     <IndexRoute component={ LandingPage } />
+    <Route path='signin' component={SigninPage} />
   </Route>
 );
