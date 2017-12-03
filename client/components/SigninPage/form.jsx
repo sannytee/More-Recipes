79df18d64f7f64bf3f7e124dd2da0bfde47ed714
Sanni Taiwo <tywosanni@gmail.com>
2017-12-03 10:48:10 +0100

    Handle form submission with state and methods from parent component

--- client/components/SigninPage/form.jsx
+++ client/components/SigninPage/form.jsx
@@ -1,48 +1,55 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import img from '../../public/images/burger-image.jpg';
 
-
-/* eslint-disable no-unused-vars */
-const Form = (props) => {
-  return (
-    <div className="container">
-      <div className="pt-5" style={{ textAlign: 'center' }}>
-        <form className="m-x-auto form card">
-          <h1 className="signin-text"> More-Recipes</h1>
-          <div>
-            <img src={img}
-              name="image"
-              className="img-fluid rounded-circle img-size"/>
+const Form = props => (
+  <div className="container">
+    <div className="pt-5" style={{ textAlign: 'center' }}>
+      <form className="m-x-auto form card" onSubmit={props.onSubmit}>
+        <h1 className="signin-text"> More-Recipes</h1>
+        <div>
+          <img src={img}
+            name="image"
+            className="img-fluid rounded-circle img-size" />
+        </div>
+        {
+          props.error.error &&
+          <div className="alert alert-danger">
+            {props.error.error}
           </div>
-          <div className="pb-4 pt-3 form-group">
-            <input
-              type="text"
-              name="username"
-              className="form-control"
-              required
-              placeholder="Username"/>
-          </div>
-          <div className="pb-4 form-group">
-            <input
-              type="password"
-              name="password"
-              className="form-control"
-              required
-              placeholder="Password"/>
-          </div>
-          <button
-            type="submit"
-            className="btn signin-btn btn-lg"
-            style={{ color: '#f2b43c' }}>
-              Log in
+        }
+        <div className="pb-4 pt-3 form-group">
+          <input
+            type="text"
+            name="username"
+            onFocus={props.onFocus}
+            onChange={props.onChange}
+            className="form-control"
+            required
+            placeholder="Username or Email" />
+        </div>
+        <div className="pb-4 form-group">
+          <input
+            type="password"
+            name="password"
+            onFocus={props.onFocus}
+            onChange={props.onChange}
+            className="form-control"
+            required
+            placeholder="Password" />
+        </div>
+        <button
+          type="submit"
+          className="btn signin-btn btn-lg"
+          style={{ color: '#f2b43c' }}>
+          Log in
           </button>
-          <p> Don’t have an account?
+        <p> Don’t have an account?
             <a href="/signup">Create an account.</a>
-          </p>
-        </form>
-      </div>
+        </p>
+      </form>
     </div>
-  );
-};
+  </div>
+);
 
 export default Form;
