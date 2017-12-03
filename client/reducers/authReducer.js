19013af2f683b10fe7ee799939101286228b8bbe
Sanni Taiwo <tywosanni@gmail.com>
2017-12-03 10:42:01 +0100

    Update the reducer to handle the action that signin users

--- client/reducers/authReducer.js
+++ client/reducers/authReducer.js
@@ -1,6 +1,7 @@
-import {  CREATE_USER } from '../actions/types';
+import { CREATE_USER, SIGN_USER } from '../actions/types';
 
 const initialState = {
+  authenticated: false,
   user: '',
   profile: {
     username: '',
@@ -8,10 +9,19 @@ const initialState = {
   }
 };
 
+
+/**
+  * @description -mpdifies the state based on action type
+  * @param  {Object} state
+  * @param  {Object} action
+  * @return {Object} returns a new state
+  */
 function authReducer(state = initialState, action) {
   switch (action.type) {
     case CREATE_USER:
-      return state;
+      return { ...state, user: action.user };
+    case SIGN_USER:
+      return { ...state, user: action.user, authenticated: true };
     default:
       return state;
   }
