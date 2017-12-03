520b6dbf7338558020ffd193bae04823dd932069
Sanni Taiwo <tywosanni@gmail.com>
2017-12-03 10:40:36 +0100

    Create the action to signin registered user

--- client/actions/authAction.js
+++ client/actions/authAction.js
@@ -1,15 +1,45 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 import axios from 'axios';
-import { CREATE_USER } from './types';
+import jwt from 'jsonwebtoken';
+import setToken from '../util/setToken';
+import { CREATE_USER, SIGN_USER } from './types';
 
 const URL = 'http://localhost:3000/api/v1';
 
-/* eslint-disable require-jsdoc */
-export default function signUpAction(userDetails) {
+/**
+  * @description - Calls the API to create new User
+  * @param  {Object} userDetails
+  * @return {Object} dispatch an object
+  */
+export function signUpAction(userDetails) {
   return dispatch => axios.post(`${URL}/users/signup`, userDetails)
     .then((res) => {
       dispatch({
         type: CREATE_USER,
         user: res.data
+      });
+    })
+    .catch((err) => {
+      throw (err);
+    });
+}
+
+/**
+  * @description - Calls the API to signin registered user
+  * @param  {Object} userDetails
+  * @return {Object} dispatches the action type and token details
+  */
+export function signinAction(userDetails) {
+  return dispatch => axios.post(`${URL}/users/signin`, userDetails)
+    .then((res) => {
+      const { token } = res.data;
+      /* eslint-disable no-undef */
+      localStorage.setItem('token', token);
+      setToken(token);
+      dispatch({
+        type: SIGN_USER,
+        user: jwt.decode(token)
       });
     })
     .catch((err) => {
