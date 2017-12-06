01871f3fa81962f2bedbd986488653d35064dc1a
Sanni Taiwo <tywosanni@gmail.com>
2017-12-06 15:40:55 +0100

    Fix linting warning

--- client/components/SigninPage/header.jsx
+++ client/components/SigninPage/header.jsx
@@ -3,32 +3,30 @@ import React from 'react';
 import { Link } from 'react-router';
 
 
-const Header = (props) => {
-  return (
-    <header>
-      <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: '#2b3034' }}>
-        <a className="navbar-brand" href="/">More-Recipes</a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+const Header = props => (
+  <header>
+    <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: '#2b3034' }}>
+      <a className="navbar-brand" href="/">More-Recipes</a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/signup">Signup</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
-  );
-}
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/signup">Signup</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+);
 
 export default Header;
