a913e7269ccb7fb7648dd8dea9e4694a8e0b2d5f
Sanni Taiwo <tywosanni@gmail.com>
2017-12-10 18:58:43 +0100

    Refactor code to properly handle invalid query parameter

--- server/src/controllers/voteController.js
+++ server/src/controllers/voteController.js
@@ -3,34 +3,35 @@ import upvoter from '../helper/upvotes';
 
 export default {
   vote(req, res) {
-    if (!req.query.action) {
+    const { action } = req.query;
+    if (!action) {
       return res.status(400).send({
         error: 'Enter a query params'
       });
     }
-    if (req.query.action !== 'upvotes' || 'downvotes') {
+    if (req.query.action) {
+      if (action === 'upvotes' || action === 'downvotes') {
+        return Recipes
+          .find({
+            where: {
+              id: req.params.recipeId,
+            }
+          })
+          .then((found) => {
+            if (!found) {
+              return res.status(404).send({
+                message: 'Recipe not found'
+              });
+            }
+            return upvoter.checkUpvotes(req, res, found);
+          })
+          .catch((err) => {
+            res.status(400).send(err);
+          });
+      }
       return res.status(400).send({
         error: 'Invalid query params'
       });
-    }
-    if (req.query.action === 'upvotes' || 'downvotes') {
-      Recipes
-        .find({
-          where: {
-            id: req.params.recipeId,
-          }
-        })
-        .then((found) => {
-          if (!found) {
-            return res.status(404).send({
-              message: 'Recipe not found'
-            });
-          }
-          return upvoter.checkUpvotes(req, res, found);
-        })
-        .catch((err) => {
-          res.status(400).send(err);
-        });
     }
   }
 };
