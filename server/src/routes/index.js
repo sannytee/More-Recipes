a22c691d98d25d9f420397c3ef7c1725a37daa5a
Sanni Taiwo <tywosanni@gmail.com>
2017-12-12 15:30:17 +0100

    Update the route for adding recipes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,13 +4,14 @@ import userController from '../controllers/userController';
 import reviewController from '../controllers/reviewController';
 import favoriteController from '../controllers/favoriteController';
 import voteController from '../controllers/voteController';
+import { checkBeforeCreatingRecipe } from '../middlewares/validation';
 
 export default (app) => {
   app.post('/api/v1/users/signup', userController.signup);
   app.post('/api/v1/users/signin', userController.signIn);
   app.get('/api/v1/recipes', recipeController.get);
   app.get('/api/v1/recipes/:recipeId', authentication.verifyUser, recipeController.getOne);
-  app.post('/api/v1/recipes', authentication.verifyUser, recipeController.addRecipe);
+  app.post('/api/v1/recipes', authentication.verifyUser, checkBeforeCreatingRecipe, recipeController.addRecipe);
   app.put('/api/v1/recipes/:recipeId', authentication.verifyUser, recipeController.updateRecipe);
   app.delete('/api/v1/recipes/:recipeId', authentication.verifyUser, recipeController.delete);
   app.post('/api/v1/recipes/:recipeId/reviews', authentication.verifyUser, reviewController.add);
