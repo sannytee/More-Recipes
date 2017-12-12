5a4fd1cb89103dac2051486209fe7dc016e92863
Sanni Taiwo <tywosanni@gmail.com>
2017-12-12 15:29:54 +0100

    Add a validation to check inputs for adding recipe

--- server/src/middlewares/validation.js
+++ server/src/middlewares/validation.js
@@ -10,3 +10,32 @@ export const checkRecipe = (res, recipe) => {
     });
   }
 };
+
+export const checkBeforeCreatingRecipe = (req, res, next) => {
+  const error = { };
+  let value;
+  if (!req.body.recipeName) {
+    error.error = 'name of recipe required';
+    value = true;
+  }
+  if (!req.body.mealType) {
+    error.error = 'mealtype required';
+    value = true;
+  }
+  if (!req.body.description) {
+    error.error = 'description required';
+    value = true;
+  }
+  if (!req.body.method) {
+    error.error = 'Method of cooking required';
+    value = true;
+  }
+  if (!req.body.ingredients) {
+    error.error = 'Input ingredients required';
+    value = true;
+  }
+  if (value === true) {
+    return res.status(400).send(error);
+  }
+  next();
+};
