1920a78149609ab871a02ea7a5294eeb60921784
Sanni Taiwo <tywosanni@gmail.com>
2017-12-12 14:53:51 +0100

    Fix eslint error

--- server/src/middlewares/validation.js
+++ server/src/middlewares/validation.js
@@ -3,10 +3,10 @@
  * @param  {object} recipe
  */
 
- export const checkRecipe = (res, recipe) => {
-    if (recipe === null) {
-      return res.status(404).send({
-        message: 'Recipe not found'
-      });
-    }
- }
+export const checkRecipe = (res, recipe) => {
+  if (recipe === null) {
+    return res.status(404).send({
+      message: 'Recipe not found'
+    });
+  }
+};
